affd52eb9990298f7ad7a15af365dd2833271363
adityanagdekar <adityanagdekar@gmail.com>
2025-11-08 16:46:57 -0500

    Adding spring boot backend

---
+++ cbs_backend/src/main/java/com/project/cbs_backend/CbsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.cbs_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbsBackendApplication.class, args);
+	}
+
+}
